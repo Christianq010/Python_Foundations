f12f3700795c9cf5f251e531f9fd4a58334a9257
Christiaan Quyn <christianq010@gmail.com>
2017-04-09 17:12:48 +0530

    refactor: Draw a circle and a Square with the Turtle Class

--- mindstorms.py
+++ mindstorms.py
@@ -1,17 +1,29 @@
 import turtle
 
 def draw_square():
+  for i in range(1,5):
+    some_turtle.forward(100)
+    some_turtle.right(90)
+
+def draw_art():    
   window = turtle.Screen()
   window.bgcolor("red")
 
+  #Create the turtle Brad - Draw a Sqaure
   brad = turtle.Turtle()
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
-  brad.right(90)
-  brad.forward(100)
+  brad.shape("turtle")
+  brad.color("yellow")
+  brad.speed(2)
+  draw_sqaure(brad)
+
+  #Create the turtle Angie - Draw a Circle
+
+  angie = turtle.Turtle()
+  angie.shape("arrow")
+  angie.color("blue")
+  angie.circle(100)
   
 
   window.exitonclick()
 
-draw_square()
+draw_art()
