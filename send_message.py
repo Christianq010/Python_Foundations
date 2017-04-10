b9a088cf2de6b03971174502a8ad0559e020e94b
Christiaan Quyn <christianq010@gmail.com>
2017-04-11 00:33:51 +0530

    feat: Send txt to verified number from Twilio

--- send_message.py
+++ send_message.py
@@ -1,2 +1,15 @@
-import twilio
-print(twilio.__version__)
+from twilio.rest import TwilioRestClient
+
+# Your Account SID from twilio.com/console
+account_sid = "AC73ac26265d35d530b6520e958eef671f"
+# Your Auth Token from twilio.com/console
+auth_token  = "96282c733dcaeb054d25da5cd1615b8f"
+
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+94777549947", 
+    from_="+17244797232",
+    body="This damn thing takes ages to arrive!")
+
+print(message.sid)
