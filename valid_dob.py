4894037704fe2fe244fa47f9de2d6b0fbb90fe0b
Christiaan Quyn <christianq010@gmail.com>
2017-04-17 10:47:48 +0530

    feat: Functions to check year/date/month validity

--- valid_dob.py
+++ valid_dob.py
@@ -33,15 +33,32 @@ months = ['January',
 # => None
 
 # Make a mapping of the first 3 letters to lower case for Months array
-# month_abbvs = dict((m[:3].lower(),m) for m in months)
+month_abbvs = dict((m[:3].lower(),m) for m in months)
 
 def valid_month(month):
     if month:
-        cap_month = month.capitalize()
-        if cap_month in months:
-            return cap_month
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
 
-print valid_month('april')
+print valid_month('April')
+
+def valid_day(day):
+    if day and day.isdigit():
+        day = int(day)
+        if day > 0 and day <= 31:
+            return day
+
+print valid_day('21')
+
+def valid_year(year):
+    if year and year.isdigit():
+        year = int(year)
+        if year > 1900 and year < 2020:
+            return year
+
+print valid_year('1985')
+
+
 
 
 
